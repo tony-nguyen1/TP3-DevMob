60c34a983d89c470dc48fc510839b6d44072a8c4
tony-nguyen1 <nguyentoony@gmail.com>
2024-03-19 17:22:11 +0100

    debut fragment

--- app/src/main/java/fr/umontpellier/etu/tp3_devmob/MainActivity.java
+++ app/src/main/java/fr/umontpellier/etu/tp3_devmob/MainActivity.java
@@ -2,6 +2,8 @@ package fr.umontpellier.etu.tp3_devmob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //UserInputFragment myFragment = new UserInputFragment();
+//        fragmentTransaction.add(,myFragment);
+        fragmentTransaction.commit();
+
+        //getSupportFragmentManager().beginTransaction()
+        //        .setReorderingAllowed(true)
+          //      .add(R.id.myLayout, UserInputFragment.class, savedInstanceState)
+            //    .commit();
+
     }
 }
