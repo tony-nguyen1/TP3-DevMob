fa8cc563d5a6b0bb42d355a577ec0ee584a85b0c
SateaMall <sate3.mallouh@gmail.com>
2024-04-05 15:46:57 +0200

    finishing saving and loading json data with upgrading the visualisations, //TODO form presistancy and avoiding the bundle

--- app/src/main/java/fr/umontpellier/etu/tp3_devmob/UserInputFragment.java
+++ app/src/main/java/fr/umontpellier/etu/tp3_devmob/UserInputFragment.java
@@ -30,7 +30,9 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -112,14 +114,14 @@ public class UserInputFragment extends Fragment {
 
         // button SAVE
         myView.findViewById(R.id.validate_button).setOnClickListener(v -> {
-            saveDataToFile();
+            saveJsonToFile();
 
         });
         // multiple choice selector
         this.set(myView);
 
 
-        myView.findViewById(R.id.upload_button).setOnClickListener(new View.OnClickListener() {
+        myView.findViewById(R.id.load_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -276,36 +278,41 @@ public class UserInputFragment extends Fragment {
         });
     }
 */
-
-    private void saveDataToFile() {
-        JSONObject userData = constructJsonFromData();
-        String filename = "userData.txt";
-
-        try (FileOutputStream fos = getActivity().openFileOutput(filename, Context.MODE_PRIVATE)) {
-            fos.write(userData.toString().getBytes());
-            Toast.makeText(getContext(), "Data saved successfully", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Log.e("DisplayFragment", "File write failed", e);
-            Toast.makeText(getContext(), "Failed to save data", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     private JSONObject constructJsonFromData() {
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("surname", name.getText().toString());
-            jsonObject.put("name", surname.getText().toString());
+            jsonObject.put("surname", surname.getText().toString());
+            jsonObject.put("name", name.getText().toString());
             jsonObject.put("birthdate", birthdate.getText().toString());
             jsonObject.put("number", number.getText().toString());
             jsonObject.put("mail", mail.getText().toString());
-            // from the TextView, get the string shown on screen
             jsonObject.put("hobbies", hobby.getText().toString());
         } catch (JSONException e) {
             Log.e("DisplayFragment", "Error creating JSON", e);
         }
         return jsonObject;
     }
+    private void saveJsonToFile() {
+        Context context = getContext();
+        if(context == null) {
+            Log.e("YourFragment", "Context is null, cannot save JSON");
+            return;
+        }
 
+        // Construct your JSON object
+        JSONObject jsonObject = constructJsonFromData();
 
+        // Get the external files directory
+        File  basePath = context.getExternalFilesDir(null);
+        File file = new File(basePath , "test.json");
+        Log.d("YourFragment", "Starting to write JSON to " + file.getAbsolutePath());
+        // Write the JSON object to the file
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(jsonObject.toString());
+            Log.d("YourFragment", "Successfully written JSON to " + file.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e("YourFragment", "Error writing JSON to file", e);
+        }
+    }
 
 }
